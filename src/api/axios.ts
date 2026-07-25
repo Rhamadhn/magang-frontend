1f3b19e290bf83506ce2d28e86877798ef780631
Rhamadhn <bramadhan891@gmail.com>
2026-07-26 00:05:43 +0700

    refactor base url using env variables

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Sesuaikan dengan URL Laravel lo
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api', // Sesuaikan dengan URL Laravel lo
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
